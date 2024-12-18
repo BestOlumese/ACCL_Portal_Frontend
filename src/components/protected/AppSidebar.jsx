1750fef040cb84479f5948a7e55c0ba2d6029c2a
Amiolemen Best <bestolumese@gmail.com>
2024-12-18 10:52:11 +0100

    update

--- src/components/protected/AppSidebar.jsx
+++ src/components/protected/AppSidebar.jsx
@@ -59,7 +59,6 @@ export default function AppSidebar() {
                         }
                       )}
                     >
-                      {/* <portal.icon /> */}
                       <span>{portal.name}</span>
                     </Link>
                   </SidebarMenuButton>
